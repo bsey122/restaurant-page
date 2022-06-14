1221ac6b0a6d839ff9c918978e2ce8f2b470d6ac
Brandon Seymour <bseymour122@yahoo.ca>
2022-06-14 18:34:17 -0400

    Add contact module

---
+++ src/contact.js
@@ -0,0 +1,30 @@
+import component from "./component";
+
+const contact = (function () {
+    const _contactHeaderText = 'Contact';
+    const _contactPhoneText = 'Call us: (123) 456 789';
+    const _contactAddressText = 'Location: 123 Fake Street, Yorknew City, USA';
+    const _contactHeadlineText = 'Come on in and try some of our world famous pizza!';
+    const _contactEmailText = 'Email: restaurant@pizza.com';
+
+    const _container = document.querySelector('#content');
+    _container.classList.add('contact');
+
+    const contactContainer = component('div', '.contact', '', 'contact-container');
+    const contactHeader = component('h2', '.contact-container', _contactHeaderText, 'contact-header');
+    const contactPhone = component('p', '.contact-container', _contactPhoneText);
+    const contactAddress = component('p', '.contact-container', _contactAddressText);
+    const contactHeadline = component('p', '.contact-container', _contactHeadlineText);
+    const contactEmail = component('p', '.contact-container', _contactEmailText);
+
+    return {
+        contactContainer,
+        contactHeader,
+        contactPhone,
+        contactAddress,
+        contactHeadline,
+        contactEmail,
+    }
+})();
+
+export default contact;
